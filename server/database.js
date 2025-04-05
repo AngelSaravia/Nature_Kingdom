c2c39086b025216044d2c93328e053f133c31f15
AustiNguyenJ <115193509+AustiNguyenJ@users.noreply.github.com>
2025-04-05 14:33:09 -0500

    Update database.js

--- server/database.js
+++ server/database.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 
 const db_connection = mysql.createConnection({
   host: "zoombase-t15.mysql.database.azure.com",
-  user: "babdi", //"livesite",
-  password: "temp", //"livesite123",
+  user: "livesite",
+  password: "livesite123",
   database: "zoo",
 });
 
